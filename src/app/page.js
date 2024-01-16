958d960c2e106f64d5c782ac4f0a2526e76267e0
monakouzi <mo.nakouzi03@gmail.com>
2024-01-15 22:46:35 -0500

    adding api call and rendering a list of events

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,46 @@
+"use client";
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
+  const [eventData, setEventData] = useState({ message: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        fetch('https://backend-events.usstm.ca/get-all')
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            setEventData(data);
+          });
+      } catch (error) {
+        console.error('Error fetching past-boards data:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <section id="homepage">
-      <h1>Home</h1>
+      {eventData.message.map((event, index) => (
+        <div key={index}>
+          <h1>{event.title}</h1>
+          <p>{event.description}</p>
+          <p>{event.date}</p>
+          <p>{event.location}</p>
+          {console.log(event.imageName)}
+          <Image
+            src={`/images/${event.imageName}`}
+            alt={event.title}
+            width={500}
+            height={500}
+          />
+          <p>{event.time}</p>
+        </div>
+      ))}
+      {(eventData.message == []) && <p>No event data is available, please check back another time.</p>}
     </section>
   )
 }
